b227e304aff21b676a94213d07a67668ca8fe465
MK <chaino0330@gmail.com>
2022-07-01 17:16:39 +0900

    적 스킬 뒤로 소환하도록 변경

--- Assets/Scripts/Stage/EnemySpawner.cs
+++ Assets/Scripts/Stage/EnemySpawner.cs
@@ -61,7 +61,7 @@ public class EnemySpawner : SingletonObject<EnemySpawner>
 		EnemyChart chart = CsvData.Ins.EnemyChart[id];
 		EnemyBase enemy = ObjectManager.Ins.Pop<EnemyBase>(Resources.Load("Prefabs/Characters/Enemies/EnemyObj" + chart.Shape.ToString()) as GameObject);
 		enemy.transform.localScale = new Vector2(chart.Size, chart.Size);
-		enemy.transform.position = new Vector2(pos.x + Random.Range(-1f,1f), pos.y + Random.Range(-1f, 1f));
+		enemy.transform.position = new Vector2(pos.x + Random.Range(-1f,1f), pos.y + Random.Range(0.2f, 2f));
 		enemy.Setup(chart, StageManager.Ins.PlayerData.Stage);
 	}
 
