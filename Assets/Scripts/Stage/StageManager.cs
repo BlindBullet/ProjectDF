d4d8e493bfc005b30beab17590d4aa89ef44e05c
MK <chaino0330@gmail.com>
2022-07-02 12:22:51 +0900

    보급품 덜 나오도록 수정

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -296,7 +296,7 @@ public class StageManager : MonoSingleton<StageManager>
 			
 			if(randNo <= appearSuppliesProb)
 			{				
-				appearSuppliesProb = -60f;
+				appearSuppliesProb = -150f;
 				return true;
 			}
 			else
