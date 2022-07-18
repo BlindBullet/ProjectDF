c459cc894d361f925b476a29543d8877223881b6
MK <chaino0330@gmail.com>
2022-07-19 03:05:58 +0900

    구글 Ver 1.2.5

--- Assets/Scripts/Stage/Projectile/ProjectileController.cs
+++ Assets/Scripts/Stage/Projectile/ProjectileController.cs
@@ -498,8 +498,8 @@ public class ProjectileData
 		MoveType = MoveType.Direct;
 		PenCount = data.Piercing;
 		Bounce = data.Bounce;
-		PushPower = data.Push + 3f;
-		PushProb = data.Push > 0 ? 7.5f : 0f;
+		PushPower = data.Push + 2f;
+		PushProb = data.Push > 0 ? 2.5f : 0f;
 		Angle = angle;
 		Speed = 20f;
 		Lifetime = 5f;
